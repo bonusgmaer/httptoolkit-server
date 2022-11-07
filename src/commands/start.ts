768e79430a17ac53a70f9860237333eab56f14f4
bonusgmaer <98702071+bonusgmaer@users.noreply.github.com>
2022-11-08 03:06:06 +0700

    bouns

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -14,7 +14,7 @@ import * as semver from 'semver';
 import { IS_PROD_BUILD } from '../constants';
 
 function maybeBundleImport<T>(moduleName: string): T {
-    if (IS_PROD_BUILD || process.env.OCLIF_TS_NODE === '0') {
+    if (false) {
         // Full built package: load the bundle
         return require('../../bundle/' + moduleName);
     } else {
